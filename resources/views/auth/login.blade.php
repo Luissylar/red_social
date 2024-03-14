2631acec3e44763c6fd07dbac37406e5de6db2f0
luis saldivar <luisfsaldivarleon@gmail.com>
2024-03-13 22:17:49 -0500

    realizando login con breeze

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -25,19 +25,23 @@
         </div>
 
         <!-- Remember Me -->
-        <div class="block mt-4">
+        <div class="flex mt-4 justify-between items-end">
             <label for="remember_me" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                <span class="ms-2 text-sm text-gray-600">{{ __('Recuerdame') }}</span>
             </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
+                    {{ __('Olvide mi contraseña?') }}
                 </a>
             @endif
+        </div>
+
+        <div class="flex items-center justify-between mt-4">
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register')  }}">
+                {{ __('Crear una cuenta') }}
+            </a>
+            
 
             <x-primary-button class="ms-3">
                 {{ __('Log in') }}
